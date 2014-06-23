78f45e5c4ab33be4e95e295228fb29eef1914d2f
Chad Barraford <cbarraford@gmail.com>
2014-06-22 22:34:23 -0400

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,10 @@ default[:dnsmasq][:enable_dhcp] = false
 default[:dnsmasq][:dhcp] = {}
 default[:dnsmasq][:dhcp_options] = []
 default[:dnsmasq][:enable_dns] = true
-default[:dnsmasq][:dns] = {}
+default[:dnsmasq][:dns] = {
+  'no-poll' => nil,
+  'server' => '127.0.0.1'
+}
 default[:dnsmasq][:dns_options] = []
 default[:dnsmasq][:managed_hosts] = {}
 default[:dnsmasq][:managed_hosts_bag] = "managed_hosts"
